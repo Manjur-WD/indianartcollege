00ec801b14b5f019a3b63b2a2d57592a8b14d9f2
Manjur-WD <mak985812@gmail.com>
2024-07-03 04:46:18 +0530

    updated

---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+$('.company-slider .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    smartSpeed: 4000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    const latestNews = document.querySelector('.latest-news');
+    const newsList = latestNews.querySelector('ul');
+    const listHeight = newsList.scrollHeight;
+    const containerHeight = latestNews.offsetHeight;
+    const midpoint = containerHeight / 2;
+    
+    let scrollInterval;
+    let topPosition = 0;
+
+    function startScrolling() {
+      scrollInterval = setInterval(() => {
+        topPosition -= 1;
+        if (Math.abs(topPosition) >= listHeight - midpoint) {
+          topPosition = 0;
+        }
+        newsList.style.top = `${topPosition}px`;
+      }, 50); // Adjust the speed by changing the interval time
+    }
+
+    function stopScrolling() {
+      clearInterval(scrollInterval);
+    }
+
+    latestNews.addEventListener('mouseenter', stopScrolling);
+    latestNews.addEventListener('mouseleave', startScrolling);
+
+    // Start the scrolling when the page loads
+    startScrolling();
+  });
